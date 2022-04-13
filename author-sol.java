2feb9a70ad341ce0aa57185967646f763d2ec02b
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-13 14:16:16 +0800

    Update author-sol.java

--- author-sol.java
+++ author-sol.java
@@ -1,1 +1,95 @@
-asddas
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for(int tc = 0; tc < T; tc++) {
+            
+            //input
+			int D = fs.nextInt(), d = fs.nextInt(), P = fs.nextInt(), Q = fs.nextInt();
+			
+			//view the image in this repository for the derivation of the formula
+			long a = P;
+			long n = D / d;
+			long diff = Q;
+			long rem = D % d;
+			
+			//related concept: arithmetic progression
+			long APSum = d * ((n * (2 * a + (n - 1) * diff)) / 2);
+			long remSum = rem * (a + n * diff);
+			long ans = APSum + remSum;
+            
+            //output
+			out.println(ans);
+            
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for(int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+		
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for(int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
